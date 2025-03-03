dd7b215ace8dafd4bc009e9b3cbcb160d344a593
Michael Chrupcala <michael@artblocks.io>
2025-03-03 15:15:57 -0500

    websocket updates. but this doesnt fully work yet

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,17 @@ server.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
+process.on("SIGINT", () => {
+  console.log("sigint");
+  // close each web socket connection before closing the DB
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin Websocket **/
 const WebSocketServer = require("ws").Server;
 
@@ -28,6 +39,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
@@ -39,3 +54,33 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/* END WEBSOCKETS */
+
+/* BEGIN DATABASE */
+const sqlite = require("sqlite3");
+
+// saving this in-memory, which means the DB will be wiped every time it's reset.
+const db = new sqlite.Database(":memory:");
+
+// this just sets up the DB before I write any queries to it
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down DB");
+  db.close();
+}
